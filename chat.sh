d718d6a95b2ad859660f927772e61366f93d7af9
IceChen <icechen128@gmail.com>
2023-07-06 19:12:24 +0800

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-PLAIN='\033[0m'
-GREEN='\033[0;32m'
-Yellow="\033[33m";
-log="unlock-chatgpt-test-result.log"
-
-echo -e "${GREEN}** Chat GPT ip可用性检测${PLAIN} ${Yellow}by JCNF·那坨${PLAIN}" && echo -e "Chat GPT ip可用性检测 by JCNF·那坨" > ${log};
-echo -e "${RED}** 提示 本工具测试结果仅供参考，请以实际使用为准${PLAIN}" && echo -e "提示 本工具测试结果仅供参考，请以实际使用为准" >> ${log};
-echo -e "** 系统时间: $(date)" && echo -e " ** 系统时间: $(date)" >> ${log};
-
 # Check if curl is installed
 if ! command -v curl &> /dev/null; then
   echo "curl is not installed. Installing curl..."
@@ -42,26 +32,26 @@ function UnlockChatGPTTest() {
     if [[ $(curl --max-time 10 -sS https://chat.openai.com/ -I | grep "text/plain") != "" ]]
     then
         local ip="$(curl -s http://checkip.dyndns.org | awk '{print $6}' | cut -d'<' -f1)"
-        echo -e " 抱歉！本机IP：${ip} ${RED}目前不支持ChatGPT IP is BLOCKED${PLAIN}" | tee -a $log
+        echo -e " No"
         return
     fi
     local countryCode="$(curl --max-time 10 -sS https://chat.openai.com/cdn-cgi/trace | grep "loc=" | awk -F= '{print $2}')";
     if [ $? -eq 1 ]; then
-        echo -e " ChatGPT: ${RED}网络连接失败 Network connection failed${PLAIN}" | tee -a $log
+        echo -e " ChatGPT: 网络连接失败 Network connection failed"
         return
     fi
     if [ -n "$countryCode" ]; then
         support_countryCodes=(T1 XX AL DZ AD AO AG AR AM AU AT AZ BS BD BB BE BZ BJ BT BA BW BR BG BF CV CA CL CO KM CR HR CY DK DJ DM DO EC SV EE FJ FI FR GA GM GE DE GH GR GD GT GN GW GY HT HN HU IS IN ID IQ IE IL IT JM JP JO KZ KE KI KW KG LV LB LS LR LI LT LU MG MW MY MV ML MT MH MR MU MX MC MN ME MA MZ MM NA NR NP NL NZ NI NE NG MK NO OM PK PW PA PG PE PH PL PT QA RO RW KN LC VC WS SM ST SN RS SC SL SG SK SI SB ZA ES LK SR SE CH TH TG TO TT TN TR TV UG AE US UY VU ZM BO BN CG CZ VA FM MD PS KR TW TZ TL GB)
         if [[ "${support_countryCodes[@]}"  =~ "${countryCode}" ]];  then
             local ip="$(curl -s http://checkip.dyndns.org | awk '{print $6}' | cut -d'<' -f1)"
-            echo -e " 恭喜！本机IP:${ip} ${GREEN}支持ChatGPT Yes (Region: ${countryCode})${PLAIN}" | tee -a $log
+            echo -e " Yes (Region: ${countryCode})"
             return
         else
-            echo -e " ChatGPT: ${RED}No${PLAIN}" | tee -a $log
+            echo -e " No"
             return
         fi
     else
-        echo -e " ChatGPT: ${RED}Failed${PLAIN}" | tee -a $log
+        echo -e " Failed"
         return
     fi
 
